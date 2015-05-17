822dae9f6472b5a6d7ccd09a205e76bdf9ff5a75
Justin Santa Barbara <justin@fathomdb.com>
2015-05-16 23:01:43 -0400

    Initialize AWSCloud correctly

--- pkg/cloudprovider/aws/aws.go
+++ pkg/cloudprovider/aws/aws.go
@@ -532,6 +532,8 @@ func newAWSCloud(config io.Reader, authFunc AuthFunc, metadata AWSMetadata) (*AW
 		region:           regionName,
 		availabilityZone: zone,
 		metadata:         metadata,
+		credentials:      credentials,
+		elbClients:       map[string]*elb.ELB{},
 	}
 
 	return awsCloud, nil
